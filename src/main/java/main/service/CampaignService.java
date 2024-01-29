0f75e8a06b5b5dc31dde14b020e0223b7f9eec86
crist <raducristina18@stud.ase.ro>
2024-01-29 10:09:28 +0200

    add all criteria MatchingCampaigns filtering

--- src/main/java/main/service/CampaignService.java
+++ src/main/java/main/service/CampaignService.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -31,11 +32,38 @@ public class CampaignService {
         List<Campaign> allCampaigns = campaignRepository.findAllByStartDateBeforeAndEndDateAfter(LocalDateTime.now(), LocalDateTime.now());
 
         return allCampaigns.stream()
-                .filter(campaign -> campaign.isEnabled()
-                                && campaign.getLevelMatchers().stream()
-                                .anyMatch(levelMatcher -> levelMatcher.getMinLevel() <= player.getLevel()
-                                        && levelMatcher.getMaxLevel() >= player.getLevel())
-                )
+                .filter(campaign -> campaign.isEnabled())
+                .filter(campaign -> checkLevelMatch(player, campaign))
+                .filter(campaign -> checkCountryMatch(player, campaign))
+                .filter(campaign -> checkItemsHave(player, campaign))
+                .filter(campaign -> checkItemsDoNotHave(player, campaign))
                 .collect(Collectors.toList());
+    }
+
+    private boolean checkLevelMatch(Player player, Campaign campaign) {
+        return campaign.getLevelMatchers().stream()
+                .anyMatch(levelMatcher -> levelMatcher.getMinLevel() <= player.getLevel()
+                        && levelMatcher.getMaxLevel() >= player.getLevel());
+    }
+
+    private boolean checkCountryMatch(Player player, Campaign campaign) {
+        return campaign.getCountries().stream()
+                .anyMatch(country -> country.getCountry().getCountryCode().equals(player.getCountry().getCountryCode()));
+    }
+
+    private boolean checkItemsHave(Player player, Campaign campaign) {
+        Set<String> playerItems = player.getInventory().stream()
+                .map(inventory -> inventory.getItem().getItemName())
+                .collect(Collectors.toSet());
+        return campaign.getItems().stream()
+                .allMatch(campaignItem -> playerItems.contains(campaignItem.getItem().getItemName()));
+    }
+
+    private boolean checkItemsDoNotHave(Player player, Campaign campaign) {
+        Set<String> playerItems = player.getInventory().stream()
+                .map(inventory -> inventory.getItem().getItemName())
+                .collect(Collectors.toSet());
+        return campaign.getDoesNotHaveItems().stream()
+                .noneMatch(campaignItem -> playerItems.contains(campaignItem.getItem().getItemName()));
     }
 }
